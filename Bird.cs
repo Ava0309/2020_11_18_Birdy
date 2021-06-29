525b065847455ece0691b966902e1f1c976f8dc7
Ava0309 <31268686+Ava0309@users.noreply.github.com>
2021-06-29 20:00:40 +0800

    Add files via upload

---
+++ Bird.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour {
+
+	public float speed=2f;
+	public float force=300f;
+
+	private void Start() {
+		GetComponent<Rigidbody2D>().velocity=
+			Vector2.right*speed;
+	}
+
+	private void Update() {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up*force);
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		// Application.LoadLevel(Application.loadedLevel);
+		SceneManager.LoadScene("Game");
+	}
+}
